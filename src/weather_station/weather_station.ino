6257909a5fec571b904c6b819598b5d1e225dc15
Slyke <slyke@phoxenix.com>
2019-01-10 21:53:40 -0500

    Added wifi connectivity

--- src/weather_station/weather_station.ino
+++ src/weather_station/weather_station.ino
@@ -2,6 +2,14 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #include <BME280I2C.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+// Wifi:
+#define STASSID "BELL457"
+#define STAPSK  "6DF2572754DF"
+#define POST_URL "http://192.168.2.23:3000/update"
+#define MAX_WIFI_TRIES 10
 
 #define SW_ENABLE_SERIAL D5
 #define SW_ENABLE_OLED D6
@@ -12,6 +20,7 @@
 #define DEEP_SLEEP_MODE deepSleep
 
 #define SERIAL_CON_SPD 19200
+#define SERIAL_DEBUG 0
 
 #define SCROLL_ENABABLED true
 
@@ -46,6 +55,11 @@ struct SensorReading_s {
   int windDirection;
   int windSpeed;
   int cps;
+  double lat;
+  double lng;
+  int alt;
+  int rssi;
+  bool rxtxConn;
 } SensorReading_UnInit {
   0,
   NAN,
@@ -56,7 +70,12 @@ struct SensorReading_s {
   0,
   0,
   0,
-  0
+  0,
+  0,
+  0,
+  0,
+  0,
+  false
 };
 
 typedef struct SensorReading_s SensorReading;
@@ -120,14 +139,39 @@ void ICACHE_FLASH_ATTR displaySensorOled(unsigned int dataToShow) {
   }
   
   if ((dataToShow & 512) == 512) {
+    display.print("Lat: ");
+    display.println(currentReading.lat);
+  }
+  
+  if ((dataToShow & 1024) == 1024) {
+    display.print("Lng: ");
+    display.println(currentReading.lat);
+  }
+
+  if ((dataToShow & 2048) == 2048) {
+    display.print("Alt: ");
+    display.println(currentReading.lat);
+  }
+
+  if ((dataToShow & 4096) == 4096) {
     display.print("RunM: ");
     char hexOut[5];
     sprintf(hexOut, "%x", currentReading.runMode);
     display.println(hexOut);
   }
 
-  if ((dataToShow & 1024) == 1024) {
-    display.println(" ");
+  if ((dataToShow & 8192) == 8192) {
+    display.print("RXTXConn: ");
+    display.println(currentReading.rxtxConn ? "true" : "false");
+  }
+
+  if ((dataToShow & 16384) == 16384) {
+    display.print("RSSI: ");
+    display.println(currentReading.rssi);
+  }
+
+  if ((dataToShow & 32768) == 32768) {
+    display.print("\n");
     display.print("Temp: ");
     display.print(currentReading.temp);
     display.println("C");
@@ -165,11 +209,21 @@ void ICACHE_FLASH_ATTR printSensorDataVerbose(Stream* client) {
   client->print("\tcps: ");
   client->print(currentReading.cps);
   client->print("c/s");
+  client->print("\tlat: ");
+  client->print(currentReading.lat);
+  client->print("\tlng: ");
+  client->print(currentReading.lng);
+  client->print("\talt: ");
+  client->print(currentReading.alt);
   client->print("\tRunM: 0x");
   if (currentReading.runMode < 0x10) {
     client->print("0");
   }
   client->print(currentReading.runMode, HEX);
+  client->print("\tRXTX: ");
+  client->print(currentReading.rxtxConn) ? "true" : "false";
+  client->print("\tRSSI: ");
+  client->print(currentReading.rssi);
   client->print(".");
 }
 
@@ -193,10 +247,27 @@ void ICACHE_FLASH_ATTR printSensorData(Stream* client) {
   client->print(",");
   client->print(currentReading.cps);
   client->print(",");
+  client->print(currentReading.lat);
+  client->print(",");
+  client->print(currentReading.lng);
+  client->print(",");
+  client->print(currentReading.alt);
+  client->print(",");
   client->print(currentReading.runMode, HEX);
+  client->print(",");
+  client->print(currentReading.rxtxConn) ? "true" : "false";
+  client->print(",");
+  client->print(currentReading.rssi);
+  
 }
 
 void ICACHE_FLASH_ATTR serialPrint(Stream* client, String dataToPrint) {
+  if ((currentReading.runMode & 2) == 2) {
+    client->print(dataToPrint);
+  }
+}
+
+void ICACHE_FLASH_ATTR serialPrint(Stream* client, long dataToPrint) {
   if ((currentReading.runMode & 2) == 2) {
     client->print(dataToPrint);
   }
@@ -215,6 +286,12 @@ void ICACHE_FLASH_ATTR displayPrint(String dataToPrint) {
   }
 }
 
+void ICACHE_FLASH_ATTR displayPrint(long dataToPrint) {
+  if ((currentReading.runMode & 4) == 4) {
+    display.print(dataToPrint);
+    display.display();
+  }
+}
 void ICACHE_FLASH_ATTR displayPrint(String dataToPrint, bool clearScreen) {
   if ((currentReading.runMode & 4) == 4) {
     if (clearScreen) {
@@ -237,6 +314,43 @@ void ICACHE_FLASH_ATTR displayPrint(int dataToPrint, bool clearScreen, bool form
   }
 }
 
+bool ICACHE_FLASH_ATTR connectToWifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(STASSID, STAPSK);
+
+  int wifiTries = 0;
+
+  displayPrint("Connecting RX/TX...\n", true);
+  serialPrint(&Serial, "\nConnecting RX/TX...\n");
+
+  while (WiFi.status() != WL_CONNECTED && wifiTries < MAX_WIFI_TRIES) {
+    wifiTries++;
+    delay(1000 * wifiTries);
+    serialPrint(&Serial, ".");
+    displayPrint(".");
+  }
+  
+  if (wifiTries >= MAX_WIFI_TRIES) {
+    serialPrint(&Serial, "\nFailed to connect to wifi");
+  
+    displayPrint("\nFailed to connect to wifi");
+    delay(2000);
+    return false;
+  } else {
+    serialPrint(&Serial, "\nWifi connected: ");
+    serialPrint(&Serial, (String)WiFi.localIP());
+    serialPrint(&Serial, (long)WiFi.RSSI());
+    serialPrint(&Serial, "\tRSSI: ");
+  
+    displayPrint("Wifi connected: ", true);
+    displayPrint((String)WiFi.localIP());
+    displayPrint("RSSI: ");
+    displayPrint((long)WiFi.RSSI());
+    
+    return true;
+  }
+}
+
 void ICACHE_FLASH_ATTR getBME280Data() {
   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
   BME280::PresUnit presUnit(BME280::PresUnit_Pa);
@@ -246,6 +360,41 @@ void ICACHE_FLASH_ATTR getBME280Data() {
 
 void ICACHE_FLASH_ATTR getLightLevelData() {
   currentReading.lightLevel = analogRead(A0);
+}
+
+void ICACHE_FLASH_ATTR getGpsData() {
+  currentReading.lat = currentReading.lat;
+  currentReading.lng = currentReading.lng;
+  currentReading.alt = currentReading.alt;
+}
+
+void ICACHE_FLASH_ATTR getGeigerMullerData() {
+  currentReading.cps = currentReading.cps;
+}
+
+void ICACHE_FLASH_ATTR getVoltageData() {
+  currentReading.windDirection = currentReading.windDirection;
+  currentReading.windSpeed = currentReading.windSpeed;
+}
+
+void ICACHE_FLASH_ATTR getWindData() {
+  currentReading.vIn = currentReading.vIn;
+  currentReading.vBat = currentReading.vBat;
+}
+
+void ICACHE_FLASH_ATTR getRxTxData() {
+  currentReading.rxtxConn = WiFi.status() == WL_CONNECTED;
+  currentReading.rssi = WiFi.RSSI();
+}
+
+void ICACHE_FLASH_ATTR updateCurrentReading() {
+  getLightLevelData();
+  getBME280Data();
+  getGpsData();
+  getGeigerMullerData();
+  getVoltageData();
+  getWindData();
+  getRxTxData();
 }
 
 void ICACHE_FLASH_ATTR setRunMode() {
@@ -335,6 +484,7 @@ void ICACHE_FLASH_ATTR setup() {
 
   if ((currentReading.runMode & 2) == 2) {
     Serial.begin(SERIAL_CON_SPD);
+    Serial.setDebugOutput(SERIAL_DEBUG);
     while (!Serial) {
       ; // wait for serial port to connect. Needed for native USB port only
     }
@@ -361,6 +511,20 @@ void ICACHE_FLASH_ATTR setup() {
     }
   }
 
+  if ((currentReading.runMode & 8) == 0) {
+    bool wifiConnected;
+    
+    if ((currentReading.runMode & 1) == 1) {
+      delay(1000);
+    }
+    
+    wifiConnected = connectToWifi();
+
+    if (!wifiConnected && (currentReading.runMode & 1) == 0) {
+      ESP.DEEP_SLEEP_MODE(DEEP_SLEEP_TIMER);
+    }
+  }
+
   int rslt;
   while ((rslt = bme.begin()) != true) {
     serialPrint(&Serial, "\n Error initialising BME280 device. ");
@@ -381,11 +545,15 @@ void ICACHE_FLASH_ATTR setup() {
   settings.tempOSR = BME280::OSR_X4;
   bme.setSettings(settings);
 
-  // Do wifi stuff
+  // Do wifi connect stuff
 
   if ((currentReading.runMode & 1) == 0) {
     ESP.DEEP_SLEEP_MODE(DEEP_SLEEP_TIMER);
   }
+
+  updateCurrentReading();
+
+  // Do wifi POST stuff
 }
 
 void ICACHE_FLASH_ATTR loop() {
@@ -406,14 +574,13 @@ void ICACHE_FLASH_ATTR loop() {
       scrollCounter++;
       scrollFrame = 0;
 
-      if (scrollCounter > 7) {
+      if (scrollCounter > 12) {
         scrollCounter = 0;
       }
     }
     
-    getLightLevelData();
-    getBME280Data();
-    displaySensorOled(15 << scrollCounter);
+    updateCurrentReading();
+    displaySensorOled((unsigned int)(15 << scrollCounter));
     printSensorDataVerbose(&Serial);
   }
 }
